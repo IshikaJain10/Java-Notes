02c7c1dcc9cc99aed792f09941bb5c468730d38d
IshikaJain10 <ishika.jain2024@lpu.in>
2026-02-03 11:28:12 +0530

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class java1 
+{
+    public static void main(String[] args) {
+        Scanner insert =new Scanner(System.in);
+        System.out.print("Enter ur age: ");
+        int age =insert.nextInt() ;
+        System.out.println("my age is  " +age);
+        // insert.close();
+
+
+    }
+
+}
